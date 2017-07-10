f628eb9fc9eec4ae20ec6caea1c0ccdae519491b
zhouzhp <zhouzhp@youzu.com>
2017-07-10 14:21:14 +0800

    redis配置分拆到外部文件

--- mob-demo-service/src/main/java/com/mob/demo/Application.java
+++ mob-demo-service/src/main/java/com/mob/demo/Application.java
@@ -1,13 +1,9 @@
 package com.mob.demo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.net.InetAddress;
 
 /**
  * TODO:写描述
@@ -15,10 +11,9 @@ import java.net.InetAddress;
  * Date: 2017/6/2:11:41
  */
 
-@SpringBootApplication
-//@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = "com.mob.demo")
 @EnableScheduling
-@PropertySource(value = {"classpath:redis.yml"}/*, ignoreResourceNotFound = true*/)
+@PropertySource(value = {"classpath:redis.properties"}/*, ignoreResourceNotFound = true*/)
 public class Application {
 
     public static void main(String[] args) {
