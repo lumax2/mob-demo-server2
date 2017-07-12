5ec16fb63ac39c8f228689c2b630f7b527c0690e
zhouzhp <zhouzhp@youzu.com>
2017-07-12 15:54:02 +0800

    small fix

--- mob-demo-service/src/main/java/com/mob/demo/dao/CommentDAO.java
+++ mob-demo-service/src/main/java/com/mob/demo/dao/CommentDAO.java
@@ -2,10 +2,12 @@ package com.mob.demo.dao;
 
 import com.lamfire.mongodb.DAOSupport;
 import com.mob.demo.domain.Comment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentDAO extends DAOSupport<Comment, String> {
+
 
     public CommentDAO() {
         super("interact", "REC_INTERACT");
