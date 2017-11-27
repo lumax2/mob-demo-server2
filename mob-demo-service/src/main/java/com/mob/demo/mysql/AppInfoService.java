cd5b228a3bc2658318dfe7fcd65845adb2fcc2a9
zhouzhp <zhouzhp@youzu.com>
2017-11-27 18:34:54 +0800

    update

--- mob-demo-service/src/main/java/com/mob/demo/mysql/AppInfoService.java
+++ mob-demo-service/src/main/java/com/mob/demo/mysql/AppInfoService.java
@@ -1,40 +1,40 @@
-package com.mob.demo.mysql;
-
-import com.alibaba.dubbo.common.utils.CollectionUtils;
-import com.mob.demo.domain.App;
-import com.mob.demo.domain.AppExample;
-import com.mob.demo.mysql.mapper.AppMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-
-//import com.mob.demo.mapper.AppMapper;
-
-/**
- * @author zhouzhipeng
- */
-@org.springframework.stereotype.Service
-public class AppInfoService {
-
-    @Autowired
-    private AppMapper appMapper;
-
-
-    public App getAppByAppKey(String appkey) {
-
-        AppExample ex = new AppExample();
-        ex.createCriteria().andAppkeyEqualTo(appkey);
-        List<App> apps = appMapper.selectByExample(ex);
-
-        App app = null;
-        if (CollectionUtils.isNotEmpty(apps)) {
-            app = apps.get(0);
-            app.setAppicon(app.getAppicon());
-
-        } else {
-            return null;
-        }
-        return null;
-    }
-
-}
+//package com.mob.demo.mysql;
+//
+//import com.alibaba.dubbo.common.utils.CollectionUtils;
+//import com.mob.demo.domain.App;
+//import com.mob.demo.domain.AppExample;
+//import com.mob.demo.mysql.mapper.AppMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import java.util.List;
+//
+////import com.mob.demo.mapper.AppMapper;
+//
+///**
+// * @author zhouzhipeng
+// */
+//@org.springframework.stereotype.Service
+//public class AppInfoService {
+//
+//    @Autowired
+//    private AppMapper appMapper;
+//
+//
+//    public App getAppByAppKey(String appkey) {
+//
+//        AppExample ex = new AppExample();
+//        ex.createCriteria().andAppkeyEqualTo(appkey);
+//        List<App> apps = appMapper.selectByExample(ex);
+//
+//        App app = null;
+//        if (CollectionUtils.isNotEmpty(apps)) {
+//            app = apps.get(0);
+//            app.setAppicon(app.getAppicon());
+//
+//        } else {
+//            return null;
+//        }
+//        return null;
+//    }
+//
+//}
--- mob-demo-service/src/main/java/com/mob/demo/service/impl/AppInfoServiceImpl.java
+++ mob-demo-service/src/main/java/com/mob/demo/service/impl/AppInfoServiceImpl.java
@@ -1,41 +1,41 @@
-package com.mob.demo.service.impl;
-
-import com.alibaba.dubbo.common.utils.CollectionUtils;
-import com.mob.demo.domain.App;
-import com.mob.demo.domain.AppExample;
-//import com.mob.demo.mapper.AppMapper;
-import com.mob.demo.mysql.mapper.AppMapper;
-import com.mob.demo.service.AppInfoService;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
-
-/**
- * @author zhouzhipeng
- */
-@org.springframework.stereotype.Service
-public class AppInfoServiceImpl implements AppInfoService {
-
-    @Autowired
-    private AppMapper appMapper;
-
-
-    @Override
-    public App getAppByAppKey(String appkey) {
-
-        AppExample ex = new AppExample();
-        ex.createCriteria().andAppkeyEqualTo(appkey);
-        List<App> apps = appMapper.selectByExample(ex);
-
-        App app = null;
-        if (CollectionUtils.isNotEmpty(apps)) {
-            app = apps.get(0);
-            app.setAppicon(app.getAppicon());
-
-        } else {
-            return null;
-        }
-        return null;
-    }
-
-}
+//package com.mob.demo.service.impl;
+//
+//import com.alibaba.dubbo.common.utils.CollectionUtils;
+//import com.mob.demo.domain.App;
+//import com.mob.demo.domain.AppExample;
+////import com.mob.demo.mapper.AppMapper;
+//import com.mob.demo.mysql.mapper.AppMapper;
+//import com.mob.demo.service.AppInfoService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//import java.util.List;
+//
+///**
+// * @author zhouzhipeng
+// */
+//@org.springframework.stereotype.Service
+//public class AppInfoServiceImpl implements AppInfoService {
+//
+//    @Autowired
+//    private AppMapper appMapper;
+//
+//
+//    @Override
+//    public App getAppByAppKey(String appkey) {
+//
+//        AppExample ex = new AppExample();
+//        ex.createCriteria().andAppkeyEqualTo(appkey);
+//        List<App> apps = appMapper.selectByExample(ex);
+//
+//        App app = null;
+//        if (CollectionUtils.isNotEmpty(apps)) {
+//            app = apps.get(0);
+//            app.setAppicon(app.getAppicon());
+//
+//        } else {
+//            return null;
+//        }
+//        return null;
+//    }
+//
+//}
